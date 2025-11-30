1e77a984703c8d52ffd5f810717a4e0ff0b150a0
sirkavics <falf555@gmail.com>
2025-11-29 23:16:42 -0500

    new database and dynamic car listing information

--- src/components/ui/Car-details.jsx
+++ src/components/ui/Car-details.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import CarSpec from "./Car-spec.jsx";
 
-const CarDetails = () => {
+const CarDetails = ({ car }) => {
   return (
     <div className="car-listing__details--container">
       <div className="car-listing__details--card">
         <h1 className="car-listing__details--title">Vehicle Details</h1>
         <div className="car-listing__specs--container">
-          <CarSpec icon="gauge-high" title="MPG" value="*MPG* city, *MPG* hwy" />
-          <CarSpec icon="gas-pump" title="Fuel type" value="*FUEL TYPE*" />
-          <CarSpec icon="spray-can-sparkles" title="Exterior color" value="*CAR COLOR*" />
-          <CarSpec icon="gears" title="Transmission" value="*TRANSMISSION*" />
-          <CarSpec icon="arrows-spin" title="Drivetrain" value="*DRIVETRAIN*" />
-          <CarSpec icon="oil-can" title="Engine" value="*ENGINE TYPE*" />
-          <CarSpec icon="clipboard-list" title="Interior" value="*INTERIOR*" />
-          <CarSpec icon="user-group" title="Seating" value="*SEATING NUMBER*" />
+          <CarSpec icon="gauge-high" title="MPG" value={`${car.mpg_city} city, ${car.mpg_highway} hwy`} />
+          <CarSpec icon="gas-pump" title="Fuel type" value={car.fuel_type} />
+          <CarSpec icon="spray-can-sparkles" title="Exterior color" value={car.exterior_color} />
+          <CarSpec icon="gears" title="Transmission" value={car.transmission} />
+          <CarSpec icon="arrows-spin" title="Drivetrain" value={car.drivetrain} />
+          <CarSpec icon="oil-can" title="Engine" value={car.engine} />
+          <CarSpec icon="clipboard-list" title="Interior" value={car.interior} />
+          <CarSpec icon="user-group" title="Seating" value={car.seating} />
         </div>
       </div>
     </div>
